b34ff5ae50ffcbea31211f81ad5b4113c126cf69
nysyo <tds24004@gmail.com>
2020-03-04 15:50:16 +0900

    修正

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var y = 0;
 var z = 0;
 const a = 0.8;
 const b = 0.05;
+const defalt = {
+    x: -1,
+    y: -1,
+    z: -1
+};
 function draw_circle(x,y,z,r){
     const n = 100;
     for(var i = 0; i < n; i++){
@@ -29,27 +34,27 @@ function move_circle(x,y,z,r){
         line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y+r*Math.sin(((i-1)/n)*2*Math.PI)} ${z}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y+r*Math.sin((i/n)*2*Math.PI)} ${z}; color: #000000`);
     }
 }
-var scene = document.querySelector('a-marker');
+var scene = document.querySelector('a-scene');
 var circle_back = document.createElement("a-circle");
 var circle_front = document.createElement("a-circle");
 var bar = document.createElement('a-entity');
-bar.setAttribute("line", `start:${1-b} 0 0; end: ${1-b+a} 0 0; color: #000000`);
-draw_circle2(0,0,0,1);
-draw_circle(1-b,0,0,b);
+bar.setAttribute("line", `start:${1-b+defalt.x} ${defalt.y} ${defalt.z}; end: ${1-b+a+defalt.x} ${defalt.y} ${defalt.z}; color: #000000`);
+draw_circle2(defalt.x,defalt.y,defalt.z,1);
+draw_circle(1-b+defalt.x,defalt.y,defalt.z,b);
 scene.append(bar);
-var prePos = `${1-b+a} 0 0`
+var prePos = `${1-b+a+defalt.x} ${defalt.y} ${defalt.z}`
 function render() {
     if(t < 2*Math.PI){
         t += 0.005;
-        x = (1-b)*Math.cos(t)+a*Math.cos(t-t/b);
-        y = (1-b)*Math.sin(t)+a*Math.sin(t-t/b);
-        z = 0;
+        x = (1-b)*Math.cos(t)+a*Math.cos(t-t/b)+defalt.x;
+        y = (1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.y;
+        z = defalt.z;
         var line = document.createElement('a-entity');
         line.setAttribute("line", `start: ${prePos}; end: ${x} ${y} ${z}; color: #136FFF`);
         requestAnimationFrame(render);
         scene.append(line);
-        move_circle((1-b)*Math.cos(t),(1-b)*Math.sin(t),0,b);
-        bar.setAttribute("line", `start:${(1-b)*Math.cos(t)} ${(1-b)*Math.sin(t)} 0; end: ${(1-b)*Math.cos(t)+a*Math.cos(t-t/b)} ${(1-b)*Math.sin(t)+a*Math.sin(t-t/b)} 0;`);     
+        move_circle((1-b)*Math.cos(t)+defalt.x,(1-b)*Math.sin(t)+defalt.y,defalt.z,b);
+        bar.setAttribute("line", `start:${(1-b)*Math.cos(t)+defalt.x} ${(1-b)*Math.sin(t)+defalt.y} ${defalt.z}; end: ${(1-b)*Math.cos(t)+a*Math.cos(t-t/b)+defalt.x} ${(1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.y} ${defalt.z};`);     
         prePos = `${x} ${y} ${z}`;
     }
 }
