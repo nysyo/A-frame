aafc10d16c289ac7216e380ccf1bac7bacebf782
nysyo <tds24004@gmail.com>
2020-03-04 17:04:39 +0900

    修正

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function draw_circle(x,y,z,r){
     for(var i = 0; i < n; i++){
         var line = document.createElement('a-entity');
         line.setAttribute("id",`line${i}`);
-        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y+r*Math.sin(((i-1)/n)*2*Math.PI)} ${z}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y+r*Math.sin((i/n)*2*Math.PI)} ${z}; color: #000000`);
+        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y} ${z+r*Math.sin(((i-1)/n)*2*Math.PI)}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y} ${z+r*Math.sin((i/n)*2*Math.PI)}; color: #000000`);
         scene.append(line);
     }
 }
@@ -22,7 +22,7 @@ function draw_circle2(x,y,z,r){
     const n = 100;
     for(var i = 0; i < n; i++){
         var line = document.createElement('a-entity');
-        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y+r*Math.sin(((i-1)/n)*2*Math.PI)} ${z}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y+r*Math.sin((i/n)*2*Math.PI)} ${z}; color: #000000`);
+        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y} ${z+r*Math.sin(((i-1)/n)*2*Math.PI)}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y} ${z+r*Math.sin((i/n)*2*Math.PI)}; color: #000000`);
         scene.append(line);
     }
 }
@@ -31,7 +31,7 @@ function move_circle(x,y,z,r){
     const n = 100;
     for(var i = 0; i < n; i++){
         var line = document.getElementById(`line${i}`);
-        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y+r*Math.sin(((i-1)/n)*2*Math.PI)} ${z}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y+r*Math.sin((i/n)*2*Math.PI)} ${z}; color: #000000`);
+        line.setAttribute("line", `start: ${x + r*Math.cos(((i-1)/n)*2*Math.PI)} ${y} ${z+r*Math.sin(((i-1)/n)*2*Math.PI)}; end: ${x+r*Math.cos((i/n)*2*Math.PI)} ${y} ${z+r*Math.sin((i/n)*2*Math.PI)}; color: #000000`);
     }
 }
 var scene = document.querySelector('a-marker');
@@ -47,14 +47,14 @@ function render() {
     if(t < 2*Math.PI){
         t += 0.005;
         x = (1-b)*Math.cos(t)+a*Math.cos(t-t/b)+defalt.x;
-        y = (1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.y;
-        z = defalt.z;
+        y = defalt.y;
+        z = (1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.z;
         var line = document.createElement('a-entity');
         line.setAttribute("line", `start: ${prePos}; end: ${x} ${y} ${z}; color: #136FFF`);
         requestAnimationFrame(render);
         scene.append(line);
-        move_circle((1-b)*Math.cos(t)+defalt.x,(1-b)*Math.sin(t)+defalt.y,defalt.z,b);
-        bar.setAttribute("line", `start:${(1-b)*Math.cos(t)+defalt.x} ${(1-b)*Math.sin(t)+defalt.y} ${defalt.z}; end: ${(1-b)*Math.cos(t)+a*Math.cos(t-t/b)+defalt.x} ${(1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.y} ${defalt.z};`);     
+        move_circle((1-b)*Math.cos(t)+defalt.x,defalt.y,(1-b)*Math.sin(t)+defalt.z,b);
+        bar.setAttribute("line", `start:${(1-b)*Math.cos(t)+defalt.x} ${defalt.y} ${(1-b)*Math.sin(t)+defalt.z}; end: ${(1-b)*Math.cos(t)+a*Math.cos(t-t/b)+defalt.x} ${defalt.y} ${(1-b)*Math.sin(t)+a*Math.sin(t-t/b)+defalt.z};`);     
         prePos = `${x} ${y} ${z}`;
     }
 }
